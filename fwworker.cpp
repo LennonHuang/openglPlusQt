0896fa1d5460b1934a1b087cc3a720a0251b7f21
LennonHuang <lennonhuang2014@gmail.com>
2022-07-28 01:19:04 -0700

    add 2d camera motion

--- fwworker.cpp
+++ fwworker.cpp
@@ -1,9 +1,13 @@
 #include "fwworker.h"
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <QDebug>
 
-
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraDir = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+float fov = 45.0;
+void mouseCallback(GLFWwindow *window, double xpos, double ypos);
+void scollCallback(GLFWwindow* window, double xoffset, double yoffset);
 
 float frtTriV[] = {
     //first triangle
@@ -147,8 +151,9 @@ void fwWorker::run()
 
     //Resize window event callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetScrollCallback(window, scollCallback);
 
-    //draw our triangle
+    //Bind to the shader ID before setting matrix
     glUseProgram(shaderProgram);
     //apply model transformation
     glm::mat4 model(1.0f);
@@ -156,19 +161,20 @@ void fwWorker::run()
     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-    //view
+    //view, camera
     glm::mat4 view(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -33.0f));
+    view = glm::lookAt(cameraPos, cameraPos + cameraDir, cameraUp);
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
     //projection
     glm::mat4 proj(1.0f);
-    proj = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+    proj = glm::perspective(glm::radians(fov), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
     unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    //mouse setting, callback must be static function
+    glfwSetCursorPosCallback(window, mouseCallback);
 
     //Keep app alive
     while (!glfwWindowShouldClose(window)) {
@@ -180,6 +186,17 @@ void fwWorker::run()
 
         //draw our triangle
         glUseProgram(shaderProgram);
+        //view, camera
+        glm::mat4 view(1.0f);
+        view = glm::lookAt(cameraPos, cameraPos + cameraDir, cameraUp);
+        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        //projection
+        glm::mat4 proj(1.0f);
+        proj = glm::perspective(glm::radians(fov), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+        unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -197,4 +214,40 @@ void fwWorker::run()
     glDeleteBuffers(2, VBO);
     glDeleteProgram(shaderProgram);
     glfwTerminate();
+}
+
+void mouseCallback(GLFWwindow *window, double xpos, double ypos)
+{
+    static double xLast, yLast;
+    static double yaw = 0;
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE && glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE){
+        xLast = xpos;
+        yLast = ypos;
+        return;
+    }
+    //Holding, catch deltax and deltay
+    double resolution = 0.01;
+    double rotresolution = 0.1;
+    double deltax = (xpos - xLast)*resolution;
+    double deltay = (yLast - ypos)*resolution;
+    double rdeltax = (xpos - xLast)*rotresolution;
+    double rdeltay = (yLast - ypos)*rotresolution;
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS){
+        cameraPos.x += deltax;
+        cameraPos.y += deltay;
+    }else if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS){
+        yaw += rdeltax;
+        cameraUp.y = cos(glm::radians(yaw));
+        cameraUp.x = sin(glm::radians(yaw));
+    }
+    xLast = xpos;
+    yLast = ypos;
+}
+
+void scollCallback(GLFWwindow* window, double xoffset, double yoffset){
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
 }
--- fwworker.h
+++ fwworker.h
@@ -6,6 +6,8 @@
 #include <glm/common.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h> //make sure glad is in front of GLFW
+#include <GLFW/glfw3.h>
 
 class fwWorker : public QObject
 {
@@ -15,15 +17,12 @@ public:
     // settings
     const unsigned int screenWidth = 800;
     const unsigned int screenHeight = 600;
-    // camera
-    glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
 signals:
     void updateq();
 public slots:
     void run();
-
+private:
 };
 
 #endif // FWWORKER_H
